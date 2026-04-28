448678dabcbfb86b39cfbc59ebfb5c6fee013ad6
Sergio <sportas2004@gmail.com>
2026-04-28 13:16:27 +0200

    e5-1

---
+++ ud05/e5/src/test/java/com/example/TestAlumno.java
@@ -0,0 +1,48 @@
+package com.example;
+
+
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestAlumno {
+
+    @Test
+    void constructorSinNada() {
+        Alumno alumno = new Alumno();
+        assertNotNull(alumno);
+        assertEquals(null, alumno.getNombre());
+        assertEquals(0, alumno.getEdad());
+        assertEquals(0.0, alumno.getNotaMedia());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Sergio , 20, 9.0 ","Jose , 16, 4.0 "})
+    void constructorConParametros(String nombre, int edad, double notaMedia) {
+        Alumno alumno = new Alumno(nombre, edad, notaMedia);
+        assertNotNull(alumno);
+        assertEquals(nombre, alumno.getNombre());
+        assertEquals(edad, alumno.getEdad());
+        assertEquals(notaMedia, alumno.getNotaMedia());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Sergio , 20, 9.0 ","Jose , 16, 4.0 "})
+    void introducirParametros(String nombre, int edad, double notaMedia) {
+        Alumno alumno = new Alumno();
+        assertNotNull(alumno);
+        alumno.setNombre(nombre);
+        alumno.setEdad(edad);
+        alumno.setNotaMedia(notaMedia);
+        assertEquals(nombre, alumno.getNombre());
+        assertEquals(edad, alumno.getEdad());
+        assertEquals(notaMedia, alumno.getNotaMedia());
+    }
+
+
+
+}
---
+++ ud05/e5/src/test/java/com/example/TestAlumnoManager.java
@@ -0,0 +1,124 @@
+package com.example;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestAlumnoManager {
+
+
+ @ParameterizedTest
+    @CsvSource({"Sergio , 20, 9.0 ","Jose , 16, 4.0 "})
+    void añadirAlumno(String nombre, int edad, double notaMedia) {
+        Alumno alumno = new Alumno(nombre, edad, notaMedia);
+        assertNotNull(alumno);
+        assertEquals(nombre, alumno.getNombre());
+        assertEquals(edad, alumno.getEdad());
+        assertEquals(notaMedia, alumno.getNotaMedia());
+        AlumnoManager alumnoManager = new AlumnoManager();
+        alumnoManager.agregarAlumno(alumno);
+        assertEquals(alumno, alumnoManager.buscarAlumnoPorNombre(nombre));
+    }
+
+
+    @Test
+    void añadirAlumnoV2() {
+        Alumno alumno = new Alumno();
+        assertNotNull(alumno);
+        AlumnoManager alumnoManager = new AlumnoManager();
+        assertEquals(null,alumnoManager.buscarAlumnoPorNombre(""));
+    }
+
+    @Test
+    void añadirAlumnoV3() {
+        Alumno alumno = null;
+        AlumnoManager alumnoManager = new AlumnoManager();
+        alumnoManager.agregarAlumno(alumno);
+        ArrayList alumnos=new ArrayList<>();
+        assertEquals(alumnos,alumnoManager.getAlumnos());
+    }
+
+    @Test
+    void clasificarAlumno() {
+        Alumno alumno = null;
+        AlumnoManager alumnoManager = new AlumnoManager();
+        alumnoManager.agregarAlumno(alumno);
+        assertEquals("Alumno nulo",alumnoManager.clasificarAlumno(alumno));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Sergio , 20, 9.0 ","Jose , 16, 4.0 "})
+    void calcularMedia(String nombre, int edad, double notaMedia) {
+        Alumno alumno = new Alumno(nombre, edad, notaMedia);
+        AlumnoManager alumnoManager = new AlumnoManager();
+        alumnoManager.agregarAlumno(alumno);
+        alumnoManager.calcularMedia();
+        assertEquals(notaMedia, alumnoManager.calcularMedia());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Sergio , 20, 9.0"})
+    void calcularMediaV2(String nombre, int edad, double notaMedia) {
+        Alumno alumno = new Alumno(nombre, edad, notaMedia);
+        Alumno alumno2 = new Alumno("Jose", 16, 4.0);
+        AlumnoManager alumnoManager = new AlumnoManager();
+        alumnoManager.agregarAlumno(alumno);
+        alumnoManager.agregarAlumno(alumno2);
+        alumnoManager.calcularMedia();
+        assertEquals((notaMedia + alumno2.getNotaMedia())/2, alumnoManager.calcularMedia());
+    }
+
+    @Test
+    void calcularMediaV3() {
+        Alumno alumno = new Alumno();
+        assertNotNull(alumno);
+        AlumnoManager alumnoManager = new AlumnoManager();
+         assertEquals((0), alumnoManager.calcularMedia());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Sergio , 20, 9.0, Sobresaliente adulto","Jose , 16, 4.0, Suspenso menor"})
+    void calificarAlumno(String nombre, int edad, double notaMedia, String calificacion) {
+        Alumno alumno = new Alumno(nombre, edad, notaMedia);
+        AlumnoManager alumnoManager = new AlumnoManager();
+        alumnoManager.agregarAlumno(alumno);
+        assertEquals(calificacion, alumnoManager.clasificarAlumno(alumno));
+    }
+
+     @ParameterizedTest
+    @CsvSource({"Sergio , 20, 9.0 "})
+    void getAlumnos(String nombre, int edad, double notaMedia) {
+        Alumno alumno = new Alumno(nombre, edad, notaMedia);
+        AlumnoManager alumnoManager = new AlumnoManager();
+         Alumno alumno2 = new Alumno("Jose", 16, 4.0);
+        alumnoManager.agregarAlumno(alumno);
+        alumnoManager.agregarAlumno(alumno2);
+        ArrayList alumnos = new ArrayList<>();
+        alumnos.add(alumno);
+        alumnos.add(alumno2);
+        assertEquals(alumnos,alumnoManager.getAlumnos());
+    }
+
+       @ParameterizedTest
+    @CsvSource({"Sergio , 20, 9.0 "})
+    void setAlumnos(String nombre, int edad, double notaMedia) {
+        Alumno alumno = new Alumno(nombre, edad, notaMedia);
+        AlumnoManager alumnoManager = new AlumnoManager();
+         Alumno alumno2 = new Alumno("Jose", 16, 4.0);
+         Alumno alumno3 = new Alumno("Toreto", 18, 6.0);
+        ArrayList alumnos = new ArrayList<>();
+        alumnos.add(alumno);
+        alumnos.add(alumno2);
+        alumnos.add(alumno3);
+           alumnoManager.setAlumnos(alumnos);
+        assertEquals(alumnos,alumnoManager.getAlumnos());
+    }
+
+
+}
+
